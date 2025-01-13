c019ab3f658ac832b12dc4a47aa8747bbcb3fbab
Luka Sever <luka.sever03@gmail.com>
2025-01-13 12:53:22 +0100

    Changed dynamodb resource to client on python end

--- Python/database/db.py
+++ Python/database/db.py
@@ -1,20 +1,20 @@
 import aioboto3
 
 class DynamoDBTables:
-    _resource = None
+    _client = None
     _tables = {}
 
     @classmethod
     async def get_resource(cls):
         session = aioboto3.Session()
-        if cls._resource is None:
-            async with session.resource("dynamodb", region_name="eu-north-1") as dynamo_resource:
-                cls._resource = dynamo_resource
-        return cls._resource
+        if cls._client is None:
+            async with session.client("dynamodb", region_name="eu-north-1") as dynamo_client:
+                cls._client = dynamo_client
+        return cls._client
 
     @classmethod
     async def get_table(cls, table_name: str):
         if table_name not in cls._tables:
-            resource = await cls.get_resource()
-            cls._tables[table_name] = await resource.Table(table_name)
+            client = await cls.get_resource()
+            cls._tables[table_name] = await client.Table(table_name)
         return cls._tables[table_name]
